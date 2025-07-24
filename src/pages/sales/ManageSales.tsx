34404e1b73d920192a437ef3327c526437b87278
Felipe <feliperocha.dev27@gmail.com>
2025-07-24 16:31:34 -0300

    feat: search sales

--- src/pages/sales/ManageSales.tsx
+++ src/pages/sales/ManageSales.tsx
@@ -1,7 +1,11 @@
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { api } from "@/services/api";
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+
 
 interface ResponseSales {
     id: string;
@@ -17,6 +21,7 @@ interface ResponseSales {
 export function ManageSales() {
     const [sales, setSales] = useState<ResponseSales[]>([])
     const navigate = useNavigate()
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         api.get('/orders').then(response => setSales(response.data))
@@ -24,7 +29,22 @@ export function ManageSales() {
 
     return (
         <div className="p-5 space-y-4">
-            {sales.map((sale) => (
+            <div className="*:not-first:mt-2">
+                <Label>Pesquise o cliente desejado</Label>
+                <div className="relative">
+                    <Input
+                        className="pe-11"
+                        placeholder="Pesquisar..."
+                        type="search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <div className="text-muted-foreground pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-2">
+                        <Search size={16} />
+                    </div>
+                </div>
+            </div>
+            {sales.filter((sale) => sale.customerName.toLowerCase().includes(search.toLowerCase())).map((sale) => (
                 <Card key={sale.id} className="@container/card bg-slate-50" onClick={() => navigate(`/sale/${sale.id}`)}>
                     <CardHeader>
                         <CardDescription className="text-2xl">{sale.customerName}</CardDescription>
