9460e9a2a68cd57b7fa1177505c5b081bc76d096
Felipe <feliperocha.dev27@gmail.com>
2025-08-01 14:25:30 -0300

    feat: receitas inativas não aparecem para o user

--- src/pages/recipes/ManageRecipes.tsx
+++ src/pages/recipes/ManageRecipes.tsx
@@ -6,9 +6,10 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 interface ResponseRecipes {
-    title: string;
-    id: string;
-    price: number;
+    title: string
+    id: string
+    price: number
+    isActivite: boolean
 }
 
 
@@ -40,14 +41,14 @@ export function ManageRecipes() {
         <div className="p-5 space-y-4">
             <ButtonReturnHome />
             {recipes.map((recipe) => (
-                <Card key={recipe.id} className="@container/card bg-slate-50">
+                <Card key={recipe.id} className={`@container/card bg-slate-50 ${recipe.isActivite ? 'block' : 'hidden'}`}>
                     <CardHeader>
                         <CardDescription className="text-2xl">{recipe.title}</CardDescription>
                         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
                             {formatter.format(recipe.price)}
                         </CardTitle>
                     </CardHeader>
-                    <div className="pl-6 space-x-3">
+                    <div className="pl-6 space-x-3 mt-3">
                         <Button variant={'destructive'} onClick={() => handleDeleteRecipe(recipe.id)}>Excluir</Button>
                         <Button variant={'default'} onClick={() => navigate(`/recipes/edit/${recipe.id}`)}>Editar</Button>
                     </div>
