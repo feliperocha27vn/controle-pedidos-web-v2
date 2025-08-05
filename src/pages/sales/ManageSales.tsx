87477cc9f9bfe085a91ca617bba272833b7f0708
Felipe <feliperocha.dev27@gmail.com>
2025-08-04 21:52:29 -0300

    feat: adicionando um calendario para manejo de vendas, para buscar as vendas por uma data especifica

--- src/pages/sales/ManageSales.tsx
+++ src/pages/sales/ManageSales.tsx
@@ -1,12 +1,19 @@
 import { ButtonReturnHome } from "@/components/button-return-home";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar-rac";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { api } from "@/services/api";
 import { format } from 'date-fns';
-import { ClockAlert, Search } from "lucide-react";
+import { ChevronDownIcon, ClockAlert, Search } from "lucide-react";
 import { useEffect, useState } from "react";
+import type { DateValue } from "react-aria-components";
 import { useNavigate } from "react-router";
 
 interface Order {
@@ -25,10 +32,18 @@ export function ManageSales() {
     const [sales, setSales] = useState<Order[]>([])
     const navigate = useNavigate()
     const [search, setSearch] = useState('')
+    const [date, setDate] = useState<DateValue | null>()
 
     useEffect(() => {
-        api.get(`/orders`).then(response => setSales(response.data.orders))
+        api.get('/orders').then(response => setSales(response.data.orders))
     }, []);
+
+    useEffect(() => {
+        if (!date) return
+        const dateFormated = new Date(date.year, date.month - 1, date.day)
+        const dateOnly = dateFormated.toISOString().split('T')[0];
+        api.get('/orders-by-date', { params: { date: dateOnly } }).then(response => setSales(response.data.orders))
+    }, [date])
 
     async function handleSearch(search: string) {
         const response = await api.get('/orders/search', {
@@ -42,6 +57,11 @@ export function ManageSales() {
         const response = await api.get('/orders/pending-filter')
         setSales(response.data)
     }
+
+    const formatterMoney = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
 
     return (
         <div className="p-5 space-y-4">
@@ -66,10 +86,32 @@ export function ManageSales() {
                 </div>
             </div>
             <div>
-                <Button variant={'outline'} onClick={() => handleFiltered()}>
-                    <ClockAlert />
-                    Pendentes
-                </Button>
+                <div className="flex items-center space-x-2">
+                    <Button variant={'outline'} onClick={() => handleFiltered()}>
+                        <ClockAlert />
+                        Pendentes
+                    </Button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline">
+                                Data das vendas
+                                <ChevronDownIcon
+                                    className="-me-1 opacity-60"
+                                    size={16}
+                                    aria-hidden="true"
+                                />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="min-w-(--radix-dropdown-menu-trigger-width)">
+                            <Calendar
+                                className="rounded-md border p-2"
+                                value={date}
+                                onChange={setDate}
+                            />
+
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
             {sales?.map((sale) => (
                 <Card key={sale.id} className="@container/card bg-slate-50" onClick={() => navigate(`/sale/${sale.id}`)}>
@@ -79,7 +121,7 @@ export function ManageSales() {
                             <CardDescription className="text-lg">{format(sale.createdAt, 'dd/MM')}</CardDescription>
                         </div>
                         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                            R$ {sale.totalAmount},00
+                            {formatterMoney.format(sale.totalAmount)}
                         </CardTitle>
                     </CardHeader>
                     <CardFooter className="flex-col items-start gap-1.5 text-sm">
